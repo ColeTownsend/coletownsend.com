6094aa52b3763a33b510757fc474a0604128a9c7
Cole Townsend <cole@coletownsend.com>
2015-02-26 20:42:28 -0500

    sweetness.

--- gulpfile.js
+++ gulpfile.js
@@ -168,7 +168,7 @@ gulp.task("html", ["styles"], function () {
 // Task to upload your site to your personal GH Pages repo
 gulp.task("deploy", function () {
   // Deploys your optimized site, you can change the settings in the html task if you want to
-  return gulp.src("./site/**/*")
+  return gulp.src("./**/*")
     .pipe($.ghPages({
       // Currently only personal GitHub Pages are supported so it will upload to the master
       // branch and automatically overwrite anything that is in the directory
