a8bc57c34656f95ab785fb4cb2fe2f1760da1ba8
Cole Townsend <cole@coletownsend.com>
2015-03-10 14:08:44 -0400

    new build. still need to standardized design.

--- src/assets/javascript/no-src.js
+++ src/assets/javascript/no-src.js
@@ -1,9 +1,22 @@
 var elems = document.getElementsByTagName('*');
 for(var i=0;i < elems.length;i++){
-    var attr = (window.devicePixelRatio >= 1.2)? elems[i].getAttribute('data-2x') : elems[i].getAttribute('data-src');
-    if(attr && elems[i].tagName == 'IMG'){
-        elems[i].src = attr;
-    } else if(attr){
-        elems[i].style.cssText += 'background-image: url('+attr+')';
+    var retina = (window.devicePixelRatio >= 1.2)? elems[i].getAttribute('data-2x') : elems[i].getAttribute('data-src');
+    var small = (window.innerWidth < 641)? elems[i].getAttribute('data-small') : elems[i].getAttribute('data-src');
+    // retina
+    if(retina && elems[i].tagName == 'IMG'){
+        elems[i].src = retina;
+    }
+    else if(retina){
+        elems[i].style.cssText += 'background-image: url('+retina+')';
+    }
+    // small
+    else if(small && elems[i].tagName == 'IMG') {
+      elems[i].src = small;
+    }
+    else if(small){
+        elems[i].style.cssText += 'background-image: url('+small+')';
+    }
+    else if(small && retina && elems[i].tagName == 'IMG' ) {
+      elems[i].style.cssText += 'background-image: url('+small+')';
     }
 }
